f2062aab2a0b4cc2b4e13fffa764bb89cb32c060
Daniel Causebrook <daniel@causebrook.co.uk>
2019-04-03 22:54:28 +0100

    Install DebugKit

--- app/Config/core.php
+++ app/Config/core.php
@@ -233,12 +233,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+Configure::write('Security.salt', 'S7jaW1w5WML9DnxZWmEQs6xoxRYV35SwKdsjPRUQlwYaD3gW32uVe9i0oLIJx17n');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+Configure::write('Security.cipherSeed', '3437391376020699213024840625359969869314');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
